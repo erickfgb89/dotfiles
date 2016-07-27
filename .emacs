04b18c3ab75d93ac5955796c036a44625968d099
Erick Aldaz <erick.aldaz@manheim.com>
2016-07-27 12:53:26 -0400

    automated backup commit Wed Jul 27 12:53:26 EDT 2016

---
+++ .emacs
@@ -0,0 +1,14 @@
+
+(add-to-list 'load-path
+	     "~/.emacs.d/custom-modules/")
+
+(require 'pacman)
+(require 'hide-ui)
+(require 'ruby-rake-printer)
+(require 'indents)
+(require 'text-comments)
+(require 'zsh)
+(require 'eshell-custom)
+(require 'recent)
+(require 'margin-of-shame)
+(require 'list-gen)
---
+++ .emacs.d/custom-modules/recent.el
@@ -0,0 +1,24 @@
+(require 'recentf)
+
+;; Mostly from: https://masteringemacs.org/article/find-files-faster-recent-files-package
+
+;; 'find-file-read-only apparently uses this shortcut, but
+;; I agree that this feature sucks and we can mask it
+(global-set-key (kbd "C-x C-r") 'ido-recentf-open)
+
+;; enable recent files
+(recentf-mode t)
+
+;; limit to 50 for now
+(setq recentf-max-saved-items 50)
+
+(defun ido-recentf-open ()
+  "Use `ido-completing-read' to \\[find-file] a recent file"
+  (interactive)
+  (if (find-file (ido-completing-read "Find recent file: " recentf-list))
+      (message "Opening file...")
+    (message "Aborting")))
+
+
+(provide 'recent)
+ 
