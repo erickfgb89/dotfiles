1b590a240394a759e13ef753c5d9913aac0e493a
Erick Aldaz <erick.aldaz@manheim.com>
2016-08-10 15:00:00 -0400

    automated backup commit Wed Aug 10 15:00:00 EDT 2016

--- .emacs.d/custom-modules/backup-files.el
+++ .emacs.d/custom-modules/backup-files.el
@@ -1,9 +1,9 @@
 (setq
  backup-by-copying t
  backup-directory-alist
- '((".*" . ,temporary-file-directory))
+ `((".*" . ,temporary-file-directory))
  auto-save-file-name-transforms
- '((".*" ,temporary-file-directory t))
+ `((".*" ,temporary-file-directory t))
  delete-old-versions t
  kept-new-versions 6
  kept-old-versions 2
