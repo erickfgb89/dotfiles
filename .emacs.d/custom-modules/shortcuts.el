c97e1314fb97e4398acca823785d3a9709cd7939
Erick Aldaz <erick.aldaz@manheim.com>
2016-08-11 14:02:43 -0400

    automated backup commit Thu Aug 11 14:02:43 EDT 2016

--- .emacs.d/custom-modules/shortcuts.el
+++ .emacs.d/custom-modules/shortcuts.el
@@ -7,14 +7,15 @@
    (global-set-key (kbd (car shortcut))
 		   (cdr shortcut)))
  '(("C-M-o" . other-window)
-   ("C-M-k" . kill-buffer)
+   ("C-M-k" . kill-this-buffer)
    ("C-M-x" . kill-buffer-and-window)
    ("C-M-0" . delete-window)
    ("C-M-1" . delete-other-windows)
    ("C-M-e" . eshell)
    ("C-M-r" . ido-recentf-open)
-   ("C-M-s" . sr-speedbar-open-and-select)
-   ("C-M-f" . find-file)))
+   ("C-M-s" . sr-speedbar-open-and-select) ; from speedbar-opt (local)
+   ("C-M-f" . find-file)
+   ("C-M-s" . save-buffer)))
 
 
 
