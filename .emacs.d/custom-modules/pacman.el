06307a9d00b8e5daf3dede797ae47e7fb04280fa
Erick Aldaz <erick.aldaz@manheim.com>
2016-08-26 23:00:01 -0400

    automated backup commit Fri Aug 26 23:00:01 EDT 2016

---
+++ .emacs.d/custom-modules/pacman.el
@@ -0,0 +1,9 @@
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+
+
+(provide 'pacman)
