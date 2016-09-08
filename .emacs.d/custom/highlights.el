a1c3d5ba0528590fb0b29b7095958f1714c26b31
Erick Aldaz <erickfgb89@gmail.com>
2016-09-08 08:13:55 -0400

    automated backup commit Thu Sep  8 08:13:55 EDT 2016

--- .emacs.d/custom/highlights.el
+++ .emacs.d/custom/highlights.el
@@ -18,14 +18,24 @@
 (font-lock-add-keywords
  'text-mode '(("##.*$" . font-lock-keyword-face)))
 
-(add-hook
- 'font-lock-mode-hook
- (lambda ()
-   (setq font-lock-keywords
-	 (append font-lock-keywords
-		 '(("\t+" (0 'my-tab-face t))
-		   ("^.\\{81,\\}$" (0 'my-long-line-face t))
-		   ("[ \t]+$"      (0 'my-trailing-space-face t)))))))
+(mapc
+ (lambda (mode)
+   (add-hook
+    (make-symbol (concat (symbol-name mode) "-mode-hook"))
+    (lambda ()
+      (setq font-lock-keywords
+	    (append font-lock-keywords
+		    '(("\t+" (0 'my-tab-face t))
+		      ("^.\\{81,\\}$" (0 'my-long-line-face t))
+		      ("[ \t]+$"      (0 'my-trailing-space-face t))))))))
+ '(c
+   java
+   haskell
+   shell-script
+   ruby
+   yaml
+   javascript
+   emacs-lisp))
 
 
 
