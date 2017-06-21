c01400431a6ac0fdc573e58e6cf3647f8dd7e633
Erick Aldaz <erick.aldaz@manheim.com>
2017-06-21 16:45:37 -0400

    automated backup commit Wed Jun 21 16:45:37 EDT 2017

--- .emacs.d/custom/ecb-config.el
+++ .emacs.d/custom/ecb-config.el
@@ -1,15 +1,15 @@
 ;; Tmp-ish fix for still-active issue:
 ;; https://github.com/ecb-home/ecb/issues/10
-(defun display-buffer-at-bottom--display-buffer-at-bottom-around
-    (orig-fun &rest args)
-"Bugfix for ECB: cannot use display-buffer-at-bottom',
-calldisplay-buffer-use-some-window' instead in ECB frame."
-(if (and ecb-minor-mode (equal (selected-frame) ecb-frame))
-(apply 'display-buffer-use-some-window args)
-(apply orig-fun args)))
-(advice-add
- 'display-buffer-at-bottom :around
- #'display-buffer-at-bottom--display-buffer-at-bottom-around)
+;; (defun display-buffer-at-bottom--display-buffer-at-bottom-around
+;;     (orig-fun &rest args)
+;; "Bugfix for ECB: cannot use display-buffer-at-bottom',
+;; calldisplay-buffer-use-some-window' instead in ECB frame."
+;; (if (and ecb-minor-mode (equal (selected-frame) ecb-frame))
+;; (apply 'display-buffer-use-some-window args)
+;; (apply orig-fun args)))
+;; (advice-add
+;;  'display-buffer-at-bottom :around
+;;  #'display-buffer-at-bottom--display-buffer-at-bottom-around)
 
 (setq
  ecb-eshell-auto-activate t
@@ -18,12 +18,11 @@ calldisplay-buffer-use-some-window' instead in ECB frame."
  ecb-compile-window-height 6
  ecb-compile-window-width 'edit-window
  ecb-compile-window-temporarily-enlarge 'after-selection
- ecb-windows-width 0.25
- ecb-auto-activate f
+ ecb-windows-width 0.2
+ ecb-auto-activate 'f
  ecb-other-window-behavior 'smart
  ecb-show-sources-in-directories-buffer 'always
  auto-expand-directory-tree 'best
  ecb-eshell-enlarge-when-eshell nil
  ecb-tip-of-the-day nil)
 
-(ecb-activate)
