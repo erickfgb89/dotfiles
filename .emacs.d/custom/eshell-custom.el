308f6618e7c076c0bde94b8b8d6cedf3f55717ed
Erick Aldaz <erickfgb89@gmail.com>
2018-04-16 07:19:50 -0400

    automated backup commit Mon Apr 16 07:19:50 EDT 2018

--- .emacs.d/custom/eshell-custom.el
+++ .emacs.d/custom/eshell-custom.el
@@ -47,7 +47,8 @@
         (let ((frame 'eshell-prompt)
               (path 'eshell-ls-directory)
               (vcs 'eshell-ls-readonly)
-              (data 'dired-ignored))
+              (data 'dired-ignored)
+              (err 'dired-warning))
        (concat
         (propertize "┌─[" 'face 'eshell-prompt)
         (propertize (concat (eshell/pwd)) 'face path)
@@ -70,6 +71,13 @@
                (propertize (match-string 1 v)
                            'face data)
                (propertize "]" 'face frame))))
+        (if (not (eq 0 eshell-last-command-status))
+            (concat
+             (propertize "──[" 'face frame)
+             (propertize (number-to-string
+                          eshell-last-command-status)
+                         'face err)
+             (propertize "]" 'face frame)))
         "\n"
         (propertize "└─>" 'face frame)
         (propertize (if (= (user-uid) 0) " #" " $") 'face frame)
