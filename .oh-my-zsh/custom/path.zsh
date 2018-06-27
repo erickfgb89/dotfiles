dc9409de8a6124d763a2f5b52b0057b89672221b
Erick Aldaz <erickfgb89@gmail.com>
2018-06-27 11:08:00 -0400

    automated backup commit Wed Jun 27 11:08:00 EDT 2018

--- .oh-my-zsh/custom/path.zsh
+++ .oh-my-zsh/custom/path.zsh
@@ -2,5 +2,6 @@ PATH=~/.rbenv/shims:~/.rbenv/bin:$PATH
 PATH=$PATH:~/dev/eventer-workstation-utils/bin
 PATH=$PATH:/usr/local/bin
 PATH=$PATH:~/node_modules/.bin
+PATH=$PATH:~/bin
 PATH="/usr/local/opt/sqlite/bin:$PATH"
 export PATH
