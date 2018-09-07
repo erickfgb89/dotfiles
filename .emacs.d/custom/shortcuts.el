840565e467df719eff16f031cf303f2216a6bd9a
Erick Aldaz <erickfgb89@gmail.com>
2018-09-07 10:37:20 -0400

    automated backup commit Fri Sep  7 10:37:20 EDT 2018

--- .emacs.d/custom/shortcuts.el
+++ .emacs.d/custom/shortcuts.el
@@ -6,7 +6,7 @@
   (cons "k" 'kill-this-buffer)
   (cons "s" 'save-buffer)
   (cons "x" 'kmacro-end-or-call-macro)
-  (cons "r" 'ido-recentf-open)
+  ;(cons "r" 'ido-recentf-open)
   (cons "p s" 'sr-speedbar-select-window)
   ;(cons "f" 'find-file)
   ;(cons "b" 'switch-to-buffer)
