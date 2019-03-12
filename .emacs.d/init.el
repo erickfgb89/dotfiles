44d6a8baf36ea6a90a2099db2f6013fad99389f6
Erick Aldaz <erickfgb89@gmail.com>
2019-03-12 07:37:22 -0400

    automated backup commit Tue Mar 12 07:37:22 EDT 2019

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,12 @@
 ;; add some exta memory for startup before gc
 ;; set debug on error/quit in case we run into something bad
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (let ((gc-cons-threshold (* 25 1024 1024))
       (debug-on-error t)
       (debug-on-quit t))
