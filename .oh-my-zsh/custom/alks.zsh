6aa229e1b4e196ea0f5b90f8fe476a253516c4b4
Erick Aldaz <erickfgb89@gmail.com>
2019-08-08 08:57:08 -0400

    automated backup commit Thu Aug  8 08:57:08 EDT 2019

--- .oh-my-zsh/custom/alks.zsh
+++ .oh-my-zsh/custom/alks.zsh
@@ -1,1 +1,2 @@
 alias awsmetnp='eval $(alks sessions open -r "Admin" -a "404947348854/ALKSAdmin - awsmetnp :: Admin" -o env)'
+alias awsmancsnp="alks sessions open -f -i -o creds -a '148143246471/ALKSPowerUser - awsmancsnp' -r 'PowerUser'"
--- .oh-my-zsh/custom/node.zsh
+++ .oh-my-zsh/custom/node.zsh
@@ -1,1 +1,3 @@
-[ -s "$HOME/.nvm/nvm.sh" ] && \. "$HOME/.nvm/nvm.sh"
+# [ -s "$HOME/.nvm/nvm.sh" ] && \. "$HOME/.nvm/nvm.sh"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
+[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"
