905b71760a6b91adb781d613442238816a851966
Erick Aldaz <erickfgb89@gmail.com>
2020-01-22 11:09:36 -0500

    automated backup commit Wed Jan 22 11:09:36 EST 2020

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -28,7 +28,7 @@
  '(ecb-options-version "2.50")
  '(package-selected-packages
    (quote
-    (hyperbole yaml-mode esup helm-robe robe-mode pug-mode helm-flycheck flycheck-mode helm-ag rubocop flycheck rinari smart-mode-line request focus workgroups2 use-package undo-tree terraform-mode sx sublimity solarized-theme robe restclient recentf-ext rbenv rainbow-delimiters racket-mode puppet-mode projectile-speedbar pollen-mode neotree multiple-cursors mode-icons markdown-preview-mode magit lua-mode js2-mode howdoi highlight-chars helm-projectile haskell-mode groovy-mode god-mode geeknote flymake-ruby enh-ruby-mode elixir-mode ediprolog ecb dockerfile-mode diminish dash-at-point company-inf-ruby column-marker color-theme-solarized cl-lib-highlight caps-lock aggressive-indent ace-jump-mode ac-inf-ruby))))
+    (web-mode counsel browsh swiper hyperbole yaml-mode esup helm-robe robe-mode pug-mode helm-flycheck flycheck-mode helm-ag rubocop flycheck rinari smart-mode-line request focus workgroups2 use-package undo-tree terraform-mode sx sublimity solarized-theme robe restclient recentf-ext rbenv rainbow-delimiters racket-mode puppet-mode projectile-speedbar pollen-mode neotree multiple-cursors mode-icons markdown-preview-mode magit lua-mode js2-mode howdoi highlight-chars helm-projectile haskell-mode groovy-mode god-mode geeknote flymake-ruby enh-ruby-mode elixir-mode ediprolog ecb dockerfile-mode diminish dash-at-point company-inf-ruby column-marker color-theme-solarized cl-lib-highlight caps-lock aggressive-indent ace-jump-mode ac-inf-ruby))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
