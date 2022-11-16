d79ea46bd6463bc63f6f9e78c567e1e8516e32d1
Erick Aldaz <erick.aldaz@coxautoinc.com>
2022-11-16 17:42:28 -0500

    regular update

--- omz/.oh-my-zsh/custom/onedrive.zsh
+++ omz/.oh-my-zsh/custom/onedrive.zsh
@@ -2,7 +2,7 @@ init_dir=$(find -L ~ -maxdepth 2 -type d -path "${HOME}/OneDrive*/init")
 
 if [ ! -z "$init_dir" ]; then
   pushd "$init_dir" > /dev/null
-  for script in *; do
+  for script in *.zsh; do
     source $script
   done
   popd > /dev/null
