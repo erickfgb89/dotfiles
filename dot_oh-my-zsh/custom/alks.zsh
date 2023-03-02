2da362c490c243a50846bd85d1e202aff01602c8
Erick Aldaz <erickfgb89@gmail.com>
2023-03-02 12:05:04 -0500

    Update alks.zsh

--- dot_oh-my-zsh/custom/alks.zsh
+++ dot_oh-my-zsh/custom/alks.zsh
@@ -1,6 +1,3 @@
-alias awsmetnp='alks sessions open -f -r "Admin" -a "404947348854/ALKSAdmin - awsmetnp :: Admin" -o creds'
-alias awsmancsnp="alks sessions open -f -i -o creds -a '148143246471/ALKSPowerUser - awsmancsnp' -r 'PowerUser'"
-
 # tabtab source for alks package
 # uninstall by removing these lines
 [[ -f ~/.config/tabtab/__tabtab.zsh ]] && . ~/.config/tabtab/__tabtab.zsh || true
